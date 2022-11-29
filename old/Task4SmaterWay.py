9ff542f70312f0a69214df4122eb189bc31c75c0
Yu <76136294+Yuviyuyu@users.noreply.github.com>
2022-11-29 13:01:08 +0930

    Update and rename Task4.py to Task4SmaterWay.py

--- old/Task4SmaterWay.py
+++ old/Task4SmaterWay.py
@@ -1,17 +1,10 @@
-
-
 import re
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 # browser =webdriver.Chrome()
 
 browser = webdriver.Chrome(executable_path=r"C:\Users\hpcyi\Documents\Data Science\chromedriver.exe")  # Path to where I installed the web driver
-
 browser.get('https://itcodefair.cdu.edu.au/data-science-challenge/')
-
-
 driver= webdriver.Chrome(executable_path=r"C:\Users\hpcyi\Documents\Data Science\chromedriver.exe")
 
 ##~~~~~~~~~~~~~~~~~~`get legislation urls~~~~~~~~~~~~~~~~~~~~`####
@@ -25,9 +18,10 @@ for url in u:
 ##~~~~~~~~~~~~~~~~~~`get each legislation 's title ~~~~~~~~~~~~~~~~~~~~`####
 for u in urls:
     driver.get(u)
-    title=driver.find_element_by_xpath("html/body/h3").text
+    title=driver.find_element_by_xpath("html/body/h2").text
+    title2=driver.find_element_by_xpath("html/body/h3").text
     print(title)  
-    s_title=title.split()
+    s_title=title2.split()
     year= s_title[-1]
     print(year)
      
