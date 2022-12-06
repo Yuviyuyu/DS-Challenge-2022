fd5d357697a1022a9201d7a3a1e57a57f7041940
Yu <76136294+Yuviyuyu@users.noreply.github.com>
2022-12-06 09:34:14 +0930

    Update Submission.py

--- Submission.py
+++ Submission.py
@@ -12,17 +12,17 @@ import collections
 import easygui
 import pip._vendor.requests
 from collections import Counter
+import spacy
 # nltk.download('punkt')
-# browser =webdriver.Chrome()
 
 # -------------- Enviroment Set UP -------------- # 
+
 browser = webdriver.Chrome(executable_path=r"C:\Users\ysenz\chromedriver.exe")  # Path to chrome driver
-userUrl = easygui.enterbox("Copy paste your legal URL here: ")
-# userUrl = "http://www5.austlii.edu.au/au/legis/nsw/consol_act/capva2007347/"
+browser2 = webdriver.Chrome(executable_path=r"C:\Users\ysenz\chromedriver.exe")  # Path to chrome driver
+# userUrl = easygui.enterbox("Copy paste your legal URL here: ")
+userUrl = "http://classic.austlii.edu.au/au/legis/act/consol_act/fva2016158/index.html#s115"
 browser.get(userUrl)
-browser2 = webdriver.Chrome(executable_path=r"C:\Users\ysenz\chromedriver.exe") # Path to chrome driver
-browser2.get('https://itcodefair.cdu.edu.au/data-science-challenge/')
-driver = webdriver.Chrome(executable_path=r"C:\Users\ysenz\chromedriver.exe") # Path to chrome driver
+browser2.get(userUrl)
 
 firstPage = []
 chapters = []
@@ -36,12 +36,10 @@ sections = []
 subsections = []
 internalUrls = []
 externalUrls = []
-uncoveredUrls = []
-allLawUrls=[]
-endnote_link=[]
-titlelist=[]
-yearlist=[]
-
+uncoverUrls = []
+endnote = []
+titlelist = []
+yearlist = []
 # -------------- Define Function to check tag existence -------------- # 
 def check_exists_by_xpath(xpath):
     try:
@@ -486,20 +484,26 @@ for element in su:
 # num_sec = len(sections)
 # print("Task 2. SECTION Word Count: ", num_sec)
         
-# -------------- Find internal references and put into a list -------------- #  
-int = browser.find_elements_by_xpath("//pre/a[@href]")
+# -------------- Find internal references in first page -------------- #  
+int = browser.find_elements_by_xpath("//a[@class='autolink_findacts']")
 for element in int:
-    links = element.get_attribute("href")
-    internalUrls.append(links)
-# print(internalUrls)
+    text = element.text
+    internalUrls.append(text)
+# print(internalUrls) 
 
-# -------------- Find external references and put into a list -------------- #  
-ext = browser.find_elements_by_xpath("//a[contains(text(), 'Act') or contains(text(), 'ACT')]")
+# -------------- Find external references in first page  -------------- #  
+ext = browser.find_elements_by_xpath("//a[@class='autolink_findacts' and (contains(text(), 'Act') or contains(text(), 'ACT'))]")
 for element in ext:
     text = element.text
     externalUrls.append(text)
 # print(externalUrls)
-        
+
+# -------------- Find  all references in first page -------------- #  
+un = browser.find_elements_by_xpath("//a[@class='autolink_findacts']")
+for element in un:
+    text = element.text
+    uncoverUrls.append(text)
+       
 # -------------- Read each subpage content -------------- #   
 for url in sec_urls:
     browser.get(url)
@@ -519,7 +523,7 @@ for url in sec_urls:
             # print(text)
             sections_text.append(text)
     else:
-        tt = browser.find_elements_by_xpath("//p[@class='clause']")
+        tt = browser.find_elements_by_xpath("//p[@class='cause']")
         for element in tt:
             text = element.text
             # print(text)
@@ -540,84 +544,77 @@ for url in sec_urls:
             text = element.text
             subsections.append(text)
     
-    # -------------- Check internal cross references -------------- # 
+    # -------------- Check internal cross references in section page-------------- # 
     if (check_exists_by_xpath("//blockquote") == True):
-        url2 = browser.find_elements_by_xpath("//blockquote/a[@href]")
+        url2 = browser.find_elements_by_xpath("//blockquote/a[@class='autolink_findacts']")
         for element in url2:
-            links = element.get_attribute("href")
+            links = element.text
             internalUrls.append(links)
     else:
-        url1 = browser.find_elements_by_xpath("//p/a[@href]")
+        url1 = browser.find_elements_by_xpath("//p/a[@class='autolink_findacts']")
         for element in url1:
-            links = element.get_attribute("href")
+            links = element.text
             internalUrls.append(links)
 # print(internalUrls)
 
-  # -------------- Check external cross references -------------- # 
+  # -------------- Check external cross references in section page-------------- # 
     if (check_exists_by_xpath("//blockquote") == True):
-        url4 = browser.find_elements_by_xpath("//blockquote/a[contains(text(), 'Act') or contains(text(), 'ACT')]")
+        url4 = browser.find_elements_by_xpath("//blockquote/a[@class='autolink_findacts' and (contains(text(), 'Act') or contains(text(), 'ACT'))]")
         for element in url4:
             text = element.text
             externalUrls.append(text)
     else:
-        url3 = browser.find_elements_by_xpath("//p/a[contains(text(), 'Act') or contains(text(), 'ACT')]")
+        url3 = browser.find_elements_by_xpath("//p/a[@class='autolink_findacts' and (contains(text(), 'Act') or contains(text(), 'ACT'))]")
         for element in url3:
             text = element.text
             externalUrls.append(text)
 # print(externalUrls)
 
+# -------------- Find  all references in first page -------------- #  
+    if (check_exists_by_xpath("//blockquote") == True):
+        un1 = browser.find_elements_by_xpath("//a[@class='autolink_findacts']")
+        for element in un1:
+            text = element.text
+            uncoverUrls.append(text)
+    else:
+        un2 = browser.find_elements_by_xpath("//a[@class='autolink_findacts']")
+        for element in un2:
+            text = element.text
+            uncoverUrls.append(text)
+       
 # -------------- Define word count -------------- #      
 def countWords(listName):
-    listName = ''.join(listName).replace("--", " ").replace("\n", " ").translate(str.maketrans('','',string.punctuation))
+    listName = ''.join(listName).replace("--", " ").replace("&nbsp;", " ").replace("\n", " ").replace("—", " ").translate(str.maketrans('','',string.punctuation))
     listName_count = len(listName.split())
     return listName_count
 
 # -------------- Task 1 to 3 Word count of a legislative text  -------------- #    
-totalWords = countWords(firstPage) + countWords(sections)
-# print(totalWords)
+totalWords = countWords(firstPage) + countWords(sections) + countWords(subsections)
+print("Task 1. Total legislative words: ", totalWords)
 
-num_sec = countWords(sections)
-print("Task 2. CHAPTER Word Count: ", countWords(chapters) + num_sec, "Part", countWords(parts)+num_sec, "DIVISIONS", countWords(divisions)+num_sec, "subdivision", countWords(subdivisions)+num_sec, "Sections", num_sec)
-print("Task 3. Number of Sections: ", len(sections)) 
+print("Task 3. Number of Sections: ", len(sec_urls)) 
+# print(sec_urls)
 print("Task 3. Number of Subsections: ", len(subsections))
 
 # -------------- Task 4 to 5 Enactment Year and Years/Frequencies of Amendents  -------------- # 
-# -------------- get first page URL from all availabe states -------------- #  
-u = browser2.find_elements(By.XPATH, "//div[@class='elementor-widget-container']//table//tbody//tr//td//p//a")
-for url in u:
-    url = url.get_attribute('innerHTML')
-    allLawUrls.append(url)
-# print(allLawUrls)
-# -------------- get enactment year -------------- # 
-print("Task 4. Legislation Enactment Year: ")
-for u in allLawUrls:
-    driver.get(u)
-    title=driver.find_element_by_xpath("html/body/h2").text
-    title2=driver.find_element_by_xpath("html/body/h3").text
-    print(title)  
-    s_title=title2.split()
-    year= s_title[-1]
-    print(year)
-
-# -------------- get endnotes URL -------------- #         
-if (check_exists_by_xpath("//a[contains(@href,'endnotes')]") == True):    
-    endnotes = browser.find_element_by_xpath("//a[contains(@href,'endnotes')]").get_attribute("href")
-    browser.get(endnotes)
-else:
-    print("No endnotes to find amendent hitosry!") 
-                
 
 if 'nt' in userUrl:
-    print("Task 5. Years and count the frequency of legislation amendments: ")
-    driver.get(l)
-    title=driver.find_element_by_xpath("html/body/h3").text
+    title = browser2.find_element_by_xpath("html/body/h3").text
+    s_title=title.split()
+    year= s_title[-1]
+    print("Task 4. Legislation Enactment Year: ", year)
+    
+    endnote = browser2.find_element_by_xpath("//a[contains(@href,'endnotes')]").get_attribute("href")
+    browser2.get(endnote)
+    
+    title=browser2.find_element_by_xpath("html/body/h3").text
     title_split=title.split()
     new_title=title_split[0:-4]
     new_title=(' '.join(new_title)).lower()
     
-    # print(new_title)
+    print(new_title)
 
-    abbr= driver.find_elements(By.XPATH, "html//body//p//table//tbody//tr//td//p//b//i[contains(text(), new_title )]")
+    abbr= browser2.find_elements(By.XPATH, "html//body//p//table//tbody//tr//td//p//b//i[contains(text(), new_title )]")
     for element in abbr:
         text=element.text
         titlelist.append(text)
@@ -629,8 +626,15 @@ if 'nt' in userUrl:
     print("List of Years of amendents in NT: ", yearlist)
     
 elif 'vic' in userUrl:
-    print("Task 5. Years and count the frequency of legislation amendments: ")
-    b_list= browser.find_elements(By.XPATH, "html//body//p//font//b")
+    title = browser2.find_element_by_xpath("html/body/h3").text
+    s_title=title.split()
+    year= s_title[-1]
+    print("Task 4. Legislation Enactment Year: ", year)
+ 
+    endnote = browser2.find_element_by_xpath("//a[contains(@href,'endnotes')]").get_attribute("href")
+    browser2.get(endnote)
+    
+    b_list= browser2.find_elements(By.XPATH, "html//body//p//font//b")
     for b in b_list:
         b=b.text
         if  b != 'Family Violence Protection Act 2008':
@@ -640,14 +644,20 @@ elif 'vic' in userUrl:
     # print(amend_list)
     
     for al in amend_list:
-        
         new_li=al[-4:]
         yearlist.append(new_li)
     print("List of Years of amendents in VIC: ", yearlist)
     
 elif '2016158' in userUrl:
-    print("Task 5. Years and count the frequency of legislation amendments: ")
-    b_list= browser.find_elements(By.XPATH, "html/body/p/font/b")
+    title = browser2.find_element_by_xpath("html/body/h3").text
+    s_title=title.split()
+    year= s_title[-1]
+    print("Task 4. Legislation Enactment Year: ", year)
+    
+    endnote = browser2.find_element_by_xpath("//a[contains(@href,'endnotes')]").get_attribute("href")
+    browser2.get(endnote)
+  
+    b_list= browser2.find_elements(By.XPATH, "html/body/p/font/b")
     for b in b_list:
         b=b.text
         titlelist.append(b)
@@ -658,46 +668,65 @@ elif '2016158' in userUrl:
             if y.isnumeric():
                 if len(y)==4:
                     yearlist.append(y)
-    print("List of Years of amendents in ACT: ", yearlist)
+    print("List of Years of amendents in ACT: ", yearlist) 
+    
 else:
-    pass        
+    print("No endnotes to find amendent hitosry!") 
+    
+browser2.quit()
 
 # -------------- Task 6 Calculate Flesch Reading Ease score -------------- #    
 
-sectionSentences = ''.join(sections).replace("--", " ").replace("\n", " ")
-sectionWords = sectionSentences.translate(str.maketrans('','',string.punctuation))
-firstPageSentences = ''.join(firstPage).replace("--", " ").replace("\n", " ")
-firstPageWords = firstPageSentences.translate(str.maketrans('','',string.punctuation))
-totalWords = len(firstPageWords.split()) + countWords(sections)
+sectionSentences = '. '.join(sections)
+subsectionSentences = '.'.join(subsections)
+firstPageSentences = '.'.join(firstPage)
+# print('1st sentence', firstPageSentences)
+firstPageWords = firstPageSentences.translate(str.maketrans('','',string.punctuation)).replace("-", " ").replace("&nbsp;", " ").replace("\n", " ").replace("|", " ").replace("—", " ")
+sectionWords = sectionSentences.translate(str.maketrans('','',string.punctuation)).replace("-", " ").replace("&nbsp;", " ").replace("\n", " ").replace("|", " ").replace("—", " ")
+subsectionWords = subsectionSentences.translate(str.maketrans('','',string.punctuation)).replace("-", " ").replace("&nbsp;", " ").replace("\n", " ").replace("|", " ").replace("—", " ")
+# print('1st word', firstPageWords)
+# print(len(sent_tokenize(sectionSentences)))
+nlp = spacy.load('en_core_web_sm')
+
+# print('sec word',len(list(nlp(firstPageSentences).sents)))
+
 # print(totalWords)
+# totalSentences = len(sent_tokenize(sectionSentences)) + len(sent_tokenize(subsectionSentences)) +len(sent_tokenize(firstPageSentences))
+totalSentences = len(list(nlp(firstPageSentences).sents)) + len(list(nlp(sectionSentences).sents)) + len(list(nlp(subsectionSentences).sents))
+# print(totalSennces)
 
-totalSentences = len(sent_tokenize(firstPageSentences)) + len(sent_tokenize(sectionSentences))
-# print(totalSentences)
+firstPgSyll = 0
+sectionSyll = 0
+subsecSyll = 0
 
-totalSyllables = 0
 for word in firstPageWords.split():
     n = syllables.estimate(word)
-    totalSyllables += n
+    firstPgSyll += n
 for word in sectionWords.split():
     n = syllables.estimate(word)
-    totalSyllables += n
+    sectionSyll += n
+for word in subsectionWords.split():
+    n = syllables.estimate(word)
+    subsecSyll += n
+totalSyllables = firstPgSyll + sectionSyll + subsecSyll
 # print(totalSyllables)
 
 def FRES(totalWords, totalSentences, totalSyllables):
     score = 206.835 - 1.015*(totalWords/totalSentences) - 84.6*(totalSyllables/totalWords)
     return score
 print("Task 6. Flesch Reading Ease Score: ", FRES(totalWords, totalSentences, totalSyllables))
-print("Task 1. Total legislative words: ", totalWords)
 # -------------- Task 7 to 9 Find Internal / External / Uncovered cross-references -------------- #
 
-NumofExternal = len(externalUrls) - 1
-NumofInternal = len(internalUrls)
+NumofExternal = len(externalUrls) 
+print(externalUrls)
+NumofInternal = len(internalUrls) -NumofExternal
+print(internalUrls)
+NumofUncover = len(uncoverUrls) - NumofExternal -NumofInternal
 
 
 print("Task 7. Number of Internal Cross-references: ", NumofInternal)    
 print("Task 8. Number of External Cross-references: ", NumofExternal)
-print("Task 9. Could not define any uncovered URLs. The defination of uncovered cross-references pattern is not clear.")
+print("Task 9. Number of Uncoover Cross-references: ", NumofUncover)
 
-browser2.quit()
-driver.quit()
+
 browser.quit()
